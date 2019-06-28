38d31d137c046371c8ce11d24ae0a725f31a36dd
dangquanghienitc <dangquanghienitc@gmail.com>
2019-06-28 17:09:57 +0700

    First upload the file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></titel>
+	</head>
+	<body>
+		<?php
+		$name = "Luong Son Ba";
+		$age = 25;
+		$courses = array("ComputerNetwork","NetworkSecurity","CloudComputing");
+		echo 	"Name: " . $name . ", age: ".$age .
+			"<br/>3rd course is: " . $courses[2];
+		?>
+	</body>
+</html>
